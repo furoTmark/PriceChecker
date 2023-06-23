dcbd7221b8d801b6c7bcfc870292228e5416d1d5
Furo Tamas-Mark <furo.tamas.mark@gmail.com>
2023-06-23 20:33:01 +0300

    Update Get method

--- PriceChecker/BitcoinPriceV1.cs
+++ PriceChecker/BitcoinPriceV1.cs
@@ -1,3 +1,5 @@
+using PriceChecker.Services;
+
 namespace PriceChecker;
 
 public static class BitcoinPriceV1
@@ -8,24 +10,33 @@ public static class BitcoinPriceV1
         {
             op.OperationId = "Aggregated";
             op.Description =
-                "Provide API Endpoint that fetches the persisted bitcoin prices from the datastore during a user-specified time range";
+                "Provide API Endpoint to request the aggregated bitcoin price at a specific time point with hour accuracy.";
             return op;
         });
         group.MapGet("/persisted/{start:datetime}/{end:datetime}", GetPersisted).WithOpenApi(op =>
         {
             op.OperationId = "Persisted";
             op.Description =
-                "Provide API Endpoint to request the aggregated bitcoin price at a specific time point with hour accuracy.";
+                "Provide API Endpoint that fetches the persisted bitcoin prices from the datastore during a user-specified time range.";
             return op;
         });
         return group;
     }
 
-    private static IResult GetAggregated(DateOnly date, int hour)
+    private static async Task<IResult> GetAggregated(DateOnly date, int hour, CancellationToken cancellationToken, IEnumerable<IPriceService> priceServices)
     {
-        date.ToDateTime(new TimeOnly(hour,0));
+        var finalDateTime = date.ToDateTime(new TimeOnly(hour,0));
+        List<Task<double>> tasks = new List<Task<double>>();
+        foreach (var priceService in priceServices)
+        {
+            tasks.Add(priceService.GetPrice(finalDateTime, cancellationToken));
+        }
 
-        return Results.Ok(10);
+        var results = await Task.WhenAll(tasks);
+
+        double finalResult = results.Sum() / results.Length;
+
+        return Results.Ok(finalResult);
     }
 
     private static IResult GetPersisted(DateTime start, DateTime end)
